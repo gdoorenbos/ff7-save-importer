616915a67ab05c8db943fa455b69ee20f7ca4adb
Garrett Doorenbos <garrett.doorenbos@gmail.com>
2020-04-18 11:57:37 -0500

    feat(gen_cksum): update to get user ID programatically

--- ff7_gen_cksum.ps1
+++ ff7_gen_cksum.ps1
@@ -1,10 +1,16 @@
 $SaveFile = "save00.ff7"
-$UserID   = "20419459"
 $md5File  = "save.md5"
 
+# Get User ID
+$UserIdScript = $PSScriptRoot+"\ff7_get_user_id.ps1"
+$UserId = &$UserIdScript
+
+# Calculate checksum
+# Checksum = md5(save_file + user_id)
 Copy-Item -Path $SaveFile -Destination $md5File
 Add-Content -Path $md5File -Value $UserID -NoNewline
 $Hash = (Get-FileHash -Path $md5File -Algorithm md5).Hash.ToLower()
 Remove-Item -Path $md5File
 
+# print checksum
 Write-Output $Hash
