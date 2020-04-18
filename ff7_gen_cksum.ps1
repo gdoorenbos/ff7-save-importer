1b71824ca937c38c061013028249cf772b55fb95
Garrett Doorenbos <garrett.doorenbos@gmail.com>
2020-04-18 11:41:53 -0500

    feat(gen_cksum): clean up output and delete intermediate file

--- ff7_gen_cksum.ps1
+++ ff7_gen_cksum.ps1
@@ -1,7 +1,10 @@
-$SaveFile = "save00.ff7.pc-orig"
+$SaveFile = "save00.ff7"
 $UserID   = "20419459"
 $md5File  = "save.md5"
 
 Copy-Item -Path $SaveFile -Destination $md5File
 Add-Content -Path $md5File -Value $UserID -NoNewline
-Get-FileHash -Path $md5File -Algorithm md5
+$Hash = (Get-FileHash -Path $md5File -Algorithm md5).Hash.ToLower()
+Remove-Item -Path $md5File
+
+Write-Output $Hash
