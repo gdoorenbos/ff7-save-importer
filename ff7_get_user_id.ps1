2bb08735171abbfc455b70f282aa13b10a125fc9
Garrett Doorenbos <garrett.doorenbos@gmail.com>
2020-04-19 16:27:03 -0500

    cleanup(ps1): remove powershell scripts. they've all been rewritten in python.

--- ff7_get_user_id.ps1
+++
@@ -1,9 +0,0 @@
-$SteamDir = Get-ChildItem ([environment]::getfolderpath("mydocuments"))"Square Enix\Final Fantasy VII Steam\"
-$UserId = ""
-
-foreach ($child in $SteamDir) {
-    if ($child.Name -match "user_[0-9]+") {
-        $UserId = $child.Name.trim("user_")
-    }
-}
-Write-Output $UserId
